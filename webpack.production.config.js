6687f4c1eea58c1ac6c1dae6783b6470c94e613a
ryanlinnane <magicantler@gmail.com>
2016-12-24 23:48:58 -0500

    remove hot-loader from prod

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -17,7 +17,7 @@ module.exports = {
       {
         test:/\.js$/,
         exclude: /node_modules/,
-        loaders:["react-hot", "babel-loader"]
+        loaders:["babel-loader"]
       },
       {
         test:/\.html$/,
