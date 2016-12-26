e5f35795e21e76968e63be5f88997ed6ff2b02a6
ryanlinnane <magicantler@gmail.com>
2016-12-25 21:00:05 -0500

    max height on resume

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -6,7 +6,7 @@ export default class Resume extends Component {
   }
   render() {
     return (
-      <div style={{display:'flex', flexDirection:'column', flex:'1'}}>
+      <div style={{display:'flex', flexDirection:'column', height:'100vh'}}>
         <iframe src="http://docs.google.com/gview?url=http://static.awhoof.com/Linnane_Ryan_Resume.pdf&embedded=true"
           style={{width:'100%', flex:'1'}} frameborder="0"></iframe>
       </div>
