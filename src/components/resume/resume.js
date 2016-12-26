27c71b99e95bc29a1cf9009067e628293a6d0965
ryanlinnane <magicantler@gmail.com>
2016-12-25 20:54:08 -0500

    proper height fit for resume iframe

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -6,9 +6,9 @@ export default class Resume extends Component {
   }
   render() {
     return (
-      <div>
+      <div style={{display:'flex', flexDirection:'column', flex:'1'}}>
         <iframe src="http://docs.google.com/gview?url=http://static.awhoof.com/Linnane_Ryan_Resume.pdf&embedded=true"
-          style={{width:'100%', height:'900px'}} frameborder="0"></iframe>
+          style={{width:'100%', flex:'1'}} frameborder="0"></iframe>
       </div>
     )
   }
