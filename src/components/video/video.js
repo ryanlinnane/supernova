d57d1abb7064b572b76eb8d62615d3c3537cd77e
ryanlinnane <magicantler@gmail.com>
2016-12-25 22:34:14 -0500

    edit video proportions

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -6,8 +6,8 @@ export default class Video extends Component {
     super(props)
   }
   render() {
-    return <div style={{color:'black', margin:'auto auto'}} className={styles.videoContainer}>
-      <iframe src="https://player.vimeo.com/video/196987303" width="640" height="640" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+    return <div style={{color:'black', display:'flex', justifyContent:'center', alignItems:'center', flex:'1'}} className={styles.videoContainer}>
+      <iframe style={{maxWidth:'850px', maxHeight:'480px'}} src="https://player.vimeo.com/video/196987303" width="640" height="640" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
       </iframe>
     </div>
   }
