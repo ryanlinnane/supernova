913903ad0f15db8d44090006e44ece9704d1d7c4
ryanlinnane <magicantler@gmail.com>
2016-12-25 22:40:15 -0500

    another vid

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -6,9 +6,10 @@ export default class Video extends Component {
     super(props)
   }
   render() {
-    return <div style={{color:'black', display:'flex', justifyContent:'center', alignItems:'center', flex:'1'}} className={styles.videoContainer}>
-      <iframe style={{maxWidth:'850px', maxHeight:'480px'}} src="https://player.vimeo.com/video/196987303" width="640" height="640" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+    return <div style={{color:'black', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', flex:'1 0 auto'}} className={styles.videoContainer}>
+      <iframe style={{maxWidth:'850px', maxHeight:'480px', marginBottom:'15px'}} src="https://player.vimeo.com/video/196987303" width="640" height="640" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
       </iframe>
+      <iframe src="https://player.vimeo.com/video/135618601" width="640" height="360" style={{maxWidth:'850px', maxHeight:'480px'}} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
   }
 }
