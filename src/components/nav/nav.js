3b8d4b394b8bbf9a02cd909fa761d7c8931938e6
ryanlinnane <magicantler@gmail.com>
2017-01-28 12:18:13 -0600

    spice up title with a glow text-shadow

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -64,7 +64,7 @@ export default class Nav extends Component {
       <div style={{display:'flex', alignItems:'center',  height:'55px', flex:'0 0 auto', justifyContent:'space-between'}}>
         <div style={{display:'flex', height:'100%', alignItems:'center'}}>
           <div style={{color:'white', paddingLeft: '5px', marginRight:'3px', fontSize:'20px', fontWeight:'500', whiteSpace:'nowrap'}}>
-             <h1 className={styles.upperCase}> {/*&lt;*/}ryan linnane  {/* &#47;&gt; */}</h1>
+             <h1 className={`${styles.upperCase} ${styles.glow}`}> {/*&lt;*/}ryan linnane  {/* &#47;&gt; */}</h1>
            </div>
           {this.props.isLoading == false ? null : <img src={require('../../crazyLoading.gif')} style={{width:'30px'}}/>}
         </div>
