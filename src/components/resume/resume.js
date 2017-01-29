b7c71e8d2449f5bfda41211ff1320eaf2b86c2c9
ryanlinnane <magicantler@gmail.com>
2017-01-28 19:50:58 -0600

    load indicator for rez

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -13,24 +13,15 @@ export default class Resume extends Component {
   }
   render() {
     return (
-
-      <div style={{flex:'1', display:'flex', flexDirection:'column', overflow:'auto', alignItems:'center'}} onLoad={() => this.props.removeLoading('resume')}>
-
+      <div style={{flex:'1', display:'flex', flexDirection:'column', overflow:'auto', alignItems:'center'}}>
         <div className={styles.container}>
           <div className={styles.resumeToolbar}>
             <a href={'http://static.awhoof.com/Linnane_Ryan_Resume.pdf'} download={true} target="_blank">
-            <img src={require('./download.png')} style={{position:'absolute', top:'5px', left:'5px', width:'45px'}}/>
+            <img src={require('./download.png')} style={{position:'absolute', top:'5px', left:'5px', width:'45px'}}  onLoad={() => { this.props.removeLoading('resume') }}/>
             </a>
           </div>
           <img src="http://static.awhoof.com/Linnane_Ryan_Resume.jpg" style={{width:'100%', maxWidth:'800px'}}/>
         </div>
-      {/*
-          <div style={{width:'100%', maxWidth:'800px', height: '100px', position:'absolute', top:'0px', backgroundColor:'red'}}>
-          asdf
-            <img src={require('./download.png')} style={{position:'absolute', bottom:'0px', right:'30%'}}/>
-          </div>
-        */}
-
       </div>
     )
   }
