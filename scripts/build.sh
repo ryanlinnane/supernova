1e0134b2351689de3933a7675d437eb4a16835d0
Ryan Linnane <ryanflinnane@gmail.com>
2017-10-20 12:06:19 -0400

    .

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,5 @@ echo 'build phase'
 git fetch origin
 git reset --hard origin/master
 npm install
-node_modules/gulp/bin/gulp.js deploy
+node_modules/gulp/bin/gulp.js copy
+sudo systemctl restart landing
