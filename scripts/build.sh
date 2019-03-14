44d2b47d2531cce6c0b7bc58991f64a617e529d0
ryanlinnane <magicantler@gmail.com>
2019-03-14 15:10:06 -0300

    .

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ git reset --hard origin/master
 echo 'running npm install'
 npm install
 echo 'copying with gulp...'
-node_modules/gulp/bin/gulp.js copy
+node_modules/gulp/bin/gulp.js deploy
 echo 'restarting landing'
 sudo systemctl restart landing
